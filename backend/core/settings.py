3874e25e3db92e8839dd3416a3108f4d752aa629
EdgarEldy <ed.eldy21@live.com>
2023-09-21 20:59:19 +0200

    Reinitialize database configuration to sqlite3

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -81,12 +81,8 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'drf_db',
-        'USER': 'root',
-        'PASSWORD': '123456',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
