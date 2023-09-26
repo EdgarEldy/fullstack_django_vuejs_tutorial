c3db939bedeb26aa99a2ffac3d57fcb9ebc46d72
EdgarEldy <ed.eldy21@live.com>
2023-09-26 21:23:51 +0200

    Add product_name and unit_price properties

--- backend/app/models/product.py
+++ backend/app/models/product.py
@@ -6,3 +6,5 @@ from app.models import Category
 # Product model
 class Product(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE(), related_name="products", null=False, )
+    product_name = models.CharField(max_length=100, unique=True)
+    unit_price = models.FloatField()
