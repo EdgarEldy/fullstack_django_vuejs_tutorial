7a87773f3ce476a8ff7f710a7b27b9b604e0c45f
EdgarEldy <ed.eldy21@live.com>
2023-09-26 21:32:03 +0200

    Fixed typo

--- backend/app/models/product.py
+++ backend/app/models/product.py
@@ -5,7 +5,7 @@ from app.models import Category
 
 # Product model
 class Product(models.Model):
-    category = models.ForeignKey(Category, on_delete=models.CASCADE(), related_name="products", null=False, )
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="products", null=False, )
     product_name = models.CharField(max_length=100, unique=True)
     unit_price = models.FloatField()
 
