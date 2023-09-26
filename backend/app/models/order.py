a98a6b60d265732a9b9102c1b3b29ea1736ecdef
EdgarEldy <ed.eldy21@live.com>
2023-09-26 21:49:32 +0200

    Add relationship to Customer model

--- backend/app/models/order.py
+++ backend/app/models/order.py
@@ -1,6 +1,8 @@
 from django.db import models
 
+from app.models import Customer
+
 
 # Order model
 class Order(models.Model):
-    pass
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE, related_name="customers", null=False, )
