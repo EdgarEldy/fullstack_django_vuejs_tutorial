46915412dfc4c6c98508ff27a2f1fa82ed573e30
EdgarEldy <ed.eldy21@live.com>
2023-09-26 21:40:15 +0200

    Update Customer model: Add properties

--- backend/app/models/customer.py
+++ backend/app/models/customer.py
@@ -3,4 +3,8 @@ from django.db import models
 
 # Customer model
 class Customer(models.Model):
-    pass
+    first_name = models.CharField(unique=True, max_length=100, verbose_name="First Name")
+    last_name = models.CharField(unique=True, max_length=100, verbose_name="Last Name")
+    tel = models.CharField(unique=True, max_length=100, verbose_name="Telephone")
+    email = models.CharField(unique=True, max_length=100, verbose_name="Email")
+    address = models.CharField(max_length=100, verbose_name="Address")
