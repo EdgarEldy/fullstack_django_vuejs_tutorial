b9353b1f67030d280dc2d3b3c93ad77ef8345881
EdgarEldy <ed.eldy21@live.com>
2023-09-26 21:22:26 +0200

    Add relationship to Category model

--- backend/app/models/product.py
+++ backend/app/models/product.py
@@ -1,6 +1,8 @@
 from django.db import models
 
+from app.models import Category
+
 
 # Product model
 class Product(models.Model):
-    pass
+    category = models.ForeignKey(Category, on_delete=models.CASCADE(), related_name="products", null=False, )
