70a0e1730d382a249a837d86e191bc6e1c786583
EdgarEldy <ed.eldy21@live.com>
2023-09-27 09:48:07 +0200

    Add Category referenced model

--- backend/app/serializers/category_serializer.py
+++ backend/app/serializers/category_serializer.py
@@ -1,6 +1,9 @@
 from rest_framework import serializers
 
+from app.models import Category
+
 
 # CategorySerializer
 class CategorySerializer(serializers.ModelSerializer):
-    pass
+    class Meta:
+        model = Category
