0be1cbb2562241501071bead549c1c94a37d4171
EdgarEldy <ed.eldy21@live.com>
2023-09-29 20:51:44 +0200

    Add relationship to CategorySerializer

--- backend/app/serializers/__init__.py
+++ backend/app/serializers/__init__.py
@@ -1,2 +1,3 @@
 ## This is the seriliazers module
 from .category_serializer import CategorySerializer
+from .product_serializer import ProductSerializer
--- backend/app/serializers/product_serializer.py
+++ backend/app/serializers/product_serializer.py
@@ -1,6 +1,10 @@
 from rest_framework import serializers
 
+from app.models import Category
+from app.serializers import CategorySerializer
+
 
 # ProductSerializer
 class ProductSerializer(serializers.ModelSerializer):
-    pass
+    category_id = serializers.PrimaryKeyRelatedField(source="category", queryset=Category.objects.all())
+    category = CategorySerializer(read_only=True)
