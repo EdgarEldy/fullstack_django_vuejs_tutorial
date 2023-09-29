52cd91733e4ca2357a8ceb97a2147ab198bf58da
EdgarEldy <ed.eldy21@live.com>
2023-09-29 21:08:10 +0200

    Add relationship to Product model

--- backend/app/serializers/order_serializer.py
+++ backend/app/serializers/order_serializer.py
@@ -1,8 +1,9 @@
 from rest_framework import serializers
 
-from app.models import Customer
+from app.models import Customer, Product
 
 
 # OrderSerializer
 class OrderSerializer(serializers.ModelSerializer):
     customer_id = serializers.PrimaryKeyRelatedField(source="customer", queryset=Customer.objects.all())
+    product_id = serializers.PrimaryKeyRelatedField(source="product", queryset=Product.objects.all())
