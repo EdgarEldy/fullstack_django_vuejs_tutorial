c14f642f307f552aaf4dd9c7817e28df1e58b5e6
EdgarEldy <ed.eldy21@live.com>
2023-09-29 20:52:47 +0200

    Add meta options

--- backend/app/serializers/product_serializer.py
+++ backend/app/serializers/product_serializer.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from app.models import Category
+from app.models import Category, Product
 from app.serializers import CategorySerializer
 
 
@@ -8,3 +8,7 @@ from app.serializers import CategorySerializer
 class ProductSerializer(serializers.ModelSerializer):
     category_id = serializers.PrimaryKeyRelatedField(source="category", queryset=Category.objects.all())
     category = CategorySerializer(read_only=True)
+
+    class Meta:
+        model = Product
+        fields = ['id', 'category_id', 'product_name', 'unit_price', 'category']
