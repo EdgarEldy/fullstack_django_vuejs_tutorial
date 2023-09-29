19c0f18c9dbc24b7b89752a8c2586e49210fefe2
EdgarEldy <ed.eldy21@live.com>
2023-09-29 21:09:01 +0200

    Add customer and product properties

--- backend/app/serializers/order_serializer.py
+++ backend/app/serializers/order_serializer.py
@@ -1,9 +1,12 @@
 from rest_framework import serializers
 
 from app.models import Customer, Product
+from app.serializers import CustomerSerializer, ProductSerializer
 
 
 # OrderSerializer
 class OrderSerializer(serializers.ModelSerializer):
     customer_id = serializers.PrimaryKeyRelatedField(source="customer", queryset=Customer.objects.all())
     product_id = serializers.PrimaryKeyRelatedField(source="product", queryset=Product.objects.all())
+    customer = CustomerSerializer(read_only=True)
+    product = ProductSerializer(read_only=True)
