a3eae7ca812f986c674783bed6d4f0ba385be3a5
EdgarEldy <ed.eldy21@live.com>
2023-09-29 21:10:26 +0200

    Export OrderSerializer as a module

--- backend/app/serializers/__init__.py
+++ backend/app/serializers/__init__.py
@@ -2,3 +2,4 @@
 from .category_serializer import CategorySerializer
 from .product_serializer import ProductSerializer
 from .customer_serializer import CustomerSerializer
+from .order_serializer import OrderSerializer
