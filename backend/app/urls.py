813cd8011d6a6add6bb1324cc55de19be4df691d
EdgarEldy <ed.eldy21@live.com>
2023-10-04 11:11:40 +0200

    Register categories route: get and post methods

--- backend/app/urls.py
+++ backend/app/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from app.views import categories
+
 urlpatterns = [
     # app urls goes here...
+    path('categories', categories.CategoryListView.as_view())
 ]
