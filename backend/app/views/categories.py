63b2a948bdd91a84f80597a9a4c70636d2c78751
EdgarEldy <ed.eldy21@live.com>
2023-10-04 11:10:21 +0200

    Retrieve and create a category

--- backend/app/views/categories.py
+++ backend/app/views/categories.py
@@ -3,4 +3,9 @@ from rest_framework import generics
 from app.models import Category
 from app.serializers import CategorySerializer
 
+
 # Generic views goes here...
+# Retrieve and create
+class CategoryListView(generics.ListCreateAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
