7a95a94f26609e8ad0849429120499d15a65d12d
EdgarEldy <ed.eldy21@live.com>
2023-10-04 11:54:23 +0200

    List customers, create a customer

---
+++ backend/app/views/customers.py
@@ -0,0 +1,11 @@
+from rest_framework import generics
+
+from app.models import Customer
+from app.serializers import CustomerSerializer
+
+
+# Customers generic views goes here...
+# List customers, create a customer
+class CustomerListView(generics.ListCreateAPIView):
+    queryset = Customer.objects.all()
+    serializer_class = CustomerSerializer
