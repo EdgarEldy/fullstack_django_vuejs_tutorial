fdac9b5d7fcb7aeb9e0e1cf7be483127feff779e
EdgarEldy <ed.eldy21@live.com>
2023-10-04 11:37:09 +0200

    Retrieve products and create a new product

--- backend/app/views/products.py
+++ backend/app/views/products.py
@@ -3,4 +3,9 @@ from rest_framework import generics
 from app.models import Product
 from app.serializers import ProductSerializer
 
+
 # Products generic views goes here
+# List products, create a product
+class ProductListView(generics.ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
