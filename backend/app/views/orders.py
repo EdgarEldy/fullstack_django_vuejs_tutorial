3ae517a1c4620dc6236e376d12540ce13be627d6
EdgarEldy <ed.eldy21@live.com>
2023-10-04 12:49:27 +0200

    List orders, create a new order

--- backend/app/views/orders.py
+++ backend/app/views/orders.py
@@ -3,4 +3,9 @@ from rest_framework import generics
 from app.models import Order
 from app.serializers import OrderSerializer
 
+
 # Orders generic views goes here...
+# List orders, create a new order
+class OrderListView(generics.ListCreateAPIView):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
